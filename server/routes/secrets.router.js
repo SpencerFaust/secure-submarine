2a434a98d25250ecee306a3d005c32b71948ec0e
Spencer Faust <faus0059@umn.edu>
2019-03-13 16:16:13 -0500

    Final commit.

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -5,18 +5,18 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 
 router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
-        .then(results => {
-            if (req.user.clearance_level > 10) {
-                res.send(results.rows)
-            } else {
-                res.sendStatus(404);
-            }
-        })
-        .catch(error => {
-            console.log('Error making SELECT for secrets:', error);
-            res.sendStatus(500);
-        });
+    if (req.user.clearance_level > 10) {
+        pool.query('SELECT * FROM "secret";')
+            .then(results => {
+                res.send(results.rows);
+            })
+            .catch(error => {
+                console.log('Error making SELECT for secrets:', error);
+                res.sendStatus(500);
+            })
+    } else {
+        res.sendStatus(400)
+    };
 });
 
 module.exports = router;
